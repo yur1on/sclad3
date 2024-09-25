7c994997d6ba3ab9c89e8110a2c5f77205660293
Your Name <1Yur1on@gmail.com>
2024-09-25 21:38:55 +0300

    обновлен css

--- myproject/settings.py
+++ myproject/settings.py
@@ -138,10 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = '/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 
 MEDIA_URL = '/media/'
