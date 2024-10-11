0c502b3fcc8174b9044dc8372aa2182d29c5eaec
Your Name <1Yur1on@gmail.com>
2024-10-11 23:57:22 +0300

    Изменены стили

--- user_profile/templatetags/custom_filters.py
+++ user_profile/templatetags/custom_filters.py
@@ -1,6 +1,18 @@
 from django.template.defaultfilters import register
 
+from django import template
+
 @register.filter
 def to(value):
     return range(1, value + 1)
+
+
+
+
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(value, css_class):
+    return value.as_widget(attrs={'class': css_class})
 
