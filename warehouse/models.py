903c250f1393bafdfe6d498e9dd873f1d498e677
Your Name <1Yur1on@gmail.com>
2024-10-14 22:58:56 +0300

    обновлена логика удаления запчастей

--- warehouse/models.py
+++ warehouse/models.py
@@ -10,10 +10,18 @@ class Part(models.Model):
     color = models.CharField(max_length=50, blank=True, null=True)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)  # Установи auto_now_add=True
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.device} - {self.brand} - {self.model} - {self.part_type}"
+
+    # Переопределяем метод delete для удаления связанных изображений
+    def delete(self, using=None, keep_parents=False):
+        # Удаляем все связанные изображения перед удалением запчасти
+        for image in self.images.all():
+            image.delete()
+        super().delete(using=using, keep_parents=keep_parents)
+
 
 class PartImage(models.Model):
     part = models.ForeignKey(Part, on_delete=models.CASCADE, related_name='images')
@@ -22,7 +30,7 @@ class PartImage(models.Model):
     def __str__(self):
         return f"Image for {self.part}"
 
+    # Переопределяем метод delete для удаления файла изображения с диска
     def delete(self, using=None, keep_parents=False):
-        self.image.delete()  # Удаляет файл изображения с сервера
+        self.image.delete(save=False)  # Удаляет файл изображения с диска
         super().delete(using=using, keep_parents=keep_parents)
-
