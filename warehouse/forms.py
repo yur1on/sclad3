a47bdc510aa156252c05edf6946bf3e2bad58f79
Your Name <1Yur1on@gmail.com>
2024-10-16 00:15:04 +0300

    добавлено поле примечание

--- warehouse/forms.py
+++ warehouse/forms.py
@@ -1,22 +1,18 @@
+
 from django import forms
 from .models import Part, PartImage
 from django.forms import modelformset_factory
-from .models import PartImage
 
 class PartForm(forms.ModelForm):
     price = forms.IntegerField(min_value=0, label="Цена")
 
     class Meta:
         model = Part
-        fields = ['device', 'brand', 'model', 'part_type', 'color', 'quantity', 'price']
-
+        fields = ['device', 'brand', 'model', 'part_type', 'color', 'quantity', 'price', 'note']  # Добавляем поле note
 
 class PartImageForm(forms.ModelForm):
     class Meta:
         model = PartImage
         fields = ['image']
 
-
-
 PartImageFormSet = modelformset_factory(PartImage, fields=('image',), extra=5)
-
--- warehouse/models.py
+++ warehouse/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db import models
+from django.contrib.auth.models import User
+
 class Part(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     device = models.CharField(max_length=100)
@@ -10,6 +13,7 @@ class Part(models.Model):
     color = models.CharField(max_length=50, blank=True, null=True)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField(blank=True, null=True)  # Поле для примечания
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
